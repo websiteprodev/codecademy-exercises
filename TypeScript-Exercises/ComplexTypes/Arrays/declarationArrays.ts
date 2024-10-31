d9fb434100e215c3a9719a2bccaa387832254934
Daniel Stoyanov <stoyanov_d@hotmail.com>
2024-10-31 23:35:27 +0200

    Add: Typescript array declaration

---
+++ TypeScript-Exercises/ComplexTypes/Arrays/declarationArrays.ts
@@ -0,0 +1,10 @@
+// Arrays:
+let bestNumbers: number[] = [7, 77, 4];
+let bestLunches: string[] = ["chicken soup", "non-chicken soup"];
+let bestBreakfasts: string[] = [
+  "scrambled eggs",
+  "oatmeal",
+  "tamago kake gohan",
+  "any kind of soup",
+];
+let bestBooleans: boolean[] = [true, false];
