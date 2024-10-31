d31a360253018df749149ef52970533cfca8b85e
Daniel Stoyanov <stoyanov_d@hotmail.com>
2024-10-31 23:51:25 +0200

    Add: declare Arrays changes

---
+++ TypeScript-Exercises/ComplexTypes/Arrays/declarationArrays.js
@@ -0,0 +1,18 @@
+// Arrays:
+var bestNumbers = [7, 77, 4];
+var bestLunches = ["chicken soup", "non-chicken soup"];
+var bestBreakfasts = [
+    "scrambled eggs",
+    "oatmeal",
+    "tamago kake gohan",
+    "any kind of soup",
+];
+var bestBooleans = [true, false];
+// Ensure TypeScript is installed globally
+// npm install -g typescript
+// Compile the TypeScript file
+// tsc declarationArrays.ts
+// If you encounter issues, ensure you are in the correct directory
+// cd /C:/Users/stoya/OneDrive/Desktop/Codeacademy/Exercises/codecademy-exercises/TypeScript-Exercises/ComplexTypes/Arrays
+// Run the compiled JavaScript file
+// node declarationArrays.js
