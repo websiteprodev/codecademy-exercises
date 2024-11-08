095719c8f082d9b1b9ed025e054a1eac47071c6e
Daniel Stoyanov <stoyanov_d@hotmail.com>
2024-11-08 13:49:15 +0200

    Add: tuples, tyepinference

---
+++ TypeScript-Exercises/ComplexTypes/Arrays/Tuples.ts
@@ -0,0 +1,10 @@
+let favoriteCoordinates: [number, number, string, number, number, string] = [
+  40,
+  43.2,
+  "N",
+  73,
+  59.8,
+  "W",
+];
+favoriteCoordinates = [17, 45, "N", 142, 30, "E"];
+favoriteCoordinates[6] = -6.825; // This is incorrect . you can not access index 6
---
+++ TypeScript-Exercises/ComplexTypes/Arrays/typeInference.ts
@@ -0,0 +1,11 @@
+// Don't change this part:
+let dogTup: [string, string, string, string] = [
+  "dog",
+  "brown fur",
+  "curly tail",
+  "sad eyes",
+];
+
+// Your code goes here:
+let myArr = dogTup.concat("happy tail");
+myArr[50] = "not a dog";
