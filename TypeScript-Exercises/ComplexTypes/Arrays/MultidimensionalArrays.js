6cfe772e08623bd6e2e70e52fb58619643e8c85d
Daniel Stoyanov <stoyanov_d@hotmail.com>
2024-11-08 11:33:19 +0200

    Add: multidimensionalArrays

---
+++ TypeScript-Exercises/ComplexTypes/Arrays/MultidimensionalArrays.js
@@ -0,0 +1,9 @@
+// Arrays:
+var bestNumbers = [7, 77, 4];
+var bestLunches = ['chicken soup', 'non-chicken soup'];
+var bestBreakfasts = ['fasting', 'oatmeal', 'tamago kake gohan', 'any kind of soup'];
+var bestBooleans = [true, false];
+// Multidimensional Arrays:
+var bestMealPlan = [bestLunches, bestBreakfasts, ['baked potato', 'mashed potato']];
+var bestBooleansTwice = [bestBooleans, bestBooleans];
+var numbersMulti = [[[1], [2, 3]], [[7], bestNumbers]];
